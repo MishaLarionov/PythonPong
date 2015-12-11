269bed5c585348b6b586c292e05e71721ef33342
Nicholas Carr <nicholas.carr@rogers.com>
2015-12-11 12:52:24 -0500

    Added general UX
    Just UX.

--- Pong.py
+++ Pong.py
@@ -1,35 +1,46 @@
 from graphics import *
 import time
 import math
+import ballmove
 window = GraphWin("Pong", 600, 600)
 window.setBackground("black")
-rpaddle = Rectangle(Point(570, 250), Point(590, 350))
-rpaddle.setFill("white")
-rpaddle.draw(window)
-"""
-#None of the below code will work until we replace GM functions with actual code
-hsp = direction * movespeed
-vsp += grav
-#Horizontal collision & movement
-if place_meeting(x + hsp, y, obj_wall):
-    while !place_meeting(x + sign(hsp), y, obj_wall):
-        x += sign(hsp)
-    hsp = 0
-    direction *= -1
-x += hsp
-#Vertical collision & movement
-if place_meeting(x, y + vsp, obj_wall):
-    while !place_meeting(x, y + sign(vsp), obj_wall):
-        y += sign(vsp)
-    vsp = 0
-    if fearofheights and !position_meeting(x + (sprite_width / 2) * direction, y + (sprite_height / 2) + 1, obj_wall):
-        dir *= -1
-y += vsp
-#Enemy collision
-if place_meeting(x, y, obj_player):
-    if obj_player.y < y - 20:
-        with(obj_player) vsp = -jumpspeed
-        instance_destroy()
-    else:
-        game_restart()
-"""
+def splashUX():
+    h1 = Text(Point(300, 50), "Welcome to PythonPong")
+    h1.setFace("helvetica")
+    h1.setSize(36)
+    h1.setTextColor("white")
+    h1.draw(window)
+    h2 = Text(Point(300, 150), "Controls")
+    h2.setFace("helvetica")
+    h2.setSize(18)
+    h2.setTextColor("white")
+    h2.draw(window)
+    p = Text(Point(300, 200), "Left: w = up, s = down; Right: i = up, k = down")
+    p.setFace("helvetica")
+    p.setSize(11)
+    p.setTextColor("white")
+    p.draw(window)
+    p2 = Text(Point(300, 550), "Click anywhere to continue")
+    p2.setFace("helvetica")
+    p2.setSize(11)
+    p2.setTextColor("white")
+    p2.draw(window)
+    window.getMouse()
+    h1.undraw()
+    h2.undraw()
+    p.undraw()
+    p2.undraw()
+    
+def gameUX():
+    lpaddle = Rectangle(Point(10, 250), Point(30, 350))
+    lpaddle.setFill("white")
+    lpaddle.draw(window)
+    rpaddle = Rectangle(Point(570, 250), Point(590, 350))
+    rpaddle.setFill("white")
+    rpaddle.draw(window)
+    ball = Circle(Point(300, 300), 10)
+    ball.setFill("grey")
+    ball.draw(window)
+    # ballmove.ballmove()
+splashUX()
+gameUX()
--- ballmove.py
+++ ballmove.py
@@ -1,3 +1,27 @@
-def ayylmao():
-    print("Ayy lmao")
-    return("5")
+def ballmove():
+    #None of the below code will work until we replace GM functions with actual code
+    hsp = direction * movespeed
+    vsp += grav
+    #Horizontal collision & movement
+    if place_meeting(x + hsp, y, obj_wall):
+        while not place_meeting(x + sign(hsp), y, obj_wall):
+            x += sign(hsp)
+        hsp = 0
+        direction *= -1
+    x += hsp
+    #Vertical collision & movement
+    if place_meeting(x, y + vsp, obj_wall):
+        while not place_meeting(x, y + sign(vsp), obj_wall):
+            y += sign(vsp)
+        vsp = 0
+        if fearofheights and not position_meeting(x + (sprite_width / 2) * direction, y + (sprite_height / 2) + 1, obj_wall):
+            dir *= -1
+    y += vsp
+    #Enemy collision
+    if place_meeting(x, y, obj_player):
+        if obj_player.y < y - 20:
+            with(obj_player):
+                vsp = -jumpspeed
+            instance_destroy()
+        else:
+            game_restart()
