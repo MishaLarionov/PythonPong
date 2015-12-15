6a25f00a684883ef6b554ccf9c669b6c99a04ca5
cheesiscool <cheesiscool@hotmail.com>
2015-12-14 19:39:34 -0500

    Acceptable "Finished" Pong (Watburg GUI).py
    Vertical movement actually makes some sense now
    Still no "new game" button; it's still messed up
    There is now an end game screen
    
    (Shut up, I got bored during French)

--- Pong(Watburg GUI).py
+++ Pong(Watburg GUI).py
@@ -4,13 +4,17 @@ import math
 import gmcode
 window = GraphWin("Pong", 600, 600)
 window.setBackground("black")
-background = Image(Point(0, 0), "background.png")
-
-background.draw(window)
 lScore = 0
 rScore = 0
 
 def splashUX():
+    background = Image(Point(0, 0), "background.png")
+    background.draw(window)
+    global lScore
+    global rScore
+    lScore = 0
+    rScore = 0
+    
     h1 = Text(Point(300, 50), "Welcome to PythonPong")
     h1.setFace("helvetica")
     h1.setSize(36)
@@ -43,9 +47,11 @@ def gameUX():
     lpaddle = Rectangle(Point(10, 250), Point(30, 350))
     lpaddle.setFill("white")
     lpaddle.draw(window)
+    
     rpaddle = Rectangle(Point(570, 250), Point(590, 350))
     rpaddle.setFill("white")
     rpaddle.draw(window)
+    
     ball = Circle(Point(300, 300), 10)
     ball.setFill("grey")
     ball.draw(window)
@@ -62,8 +68,11 @@ def gameUX():
     rScoreDisplay.setTextColor("white")
     rScoreDisplay.draw(window)
     
-    moveX = 3
+    moveX = 4
     moveY = 0
+
+    global rScore
+    global lScore
     
     while moveX != 0:
         # Input functions and logic
@@ -72,36 +81,41 @@ def gameUX():
         lpaddleY = lpaddle.getCenter().getY()
         rpaddleY = rpaddle.getCenter().getY()
         if key == "w" and lpaddleY > 50:
-            lpaddle.move(0, -7)
+            lpaddle.move(0, -8)
         elif key == "s" and lpaddleY < 550:
-            lpaddle.move(0, 7)
+            lpaddle.move(0, 8)
         elif key == "i" and rpaddleY > 50:
-            rpaddle.move(0, -7)
+            rpaddle.move(0, -8)
         elif key == "k" and rpaddleY < 550:
-            rpaddle.move(0, 7)
+            rpaddle.move(0, 8)
+
+        #collisions
         if ball.getCenter().getX() >= 560:
+            
             if rpaddleY + 60 > ball.getCenter().getY() > rpaddleY - 60:
-                moveY = moveY- (ball.getCenter().getY() - rpaddleY)/4
-                if moveY>6:
-                    moveY = 6
-                moveX = -moveX
+                moveY = -(moveY- (ball.getCenter().getY() - rpaddleY)/14)
+                moveX = -(abs((15 - moveY**2)**0.5))
+                
             else:
-                global lScore
                 lScore = lScore + 1
                 moveX = 0
+                
         if ball.getCenter().getX() <= 40:
+            
             if lpaddleY + 60 > ball.getCenter().getY() > lpaddleY - 60:
-                moveY = moveY- (ball.getCenter().getY() - lpaddleY)/4
-                if moveY>6:
-                    moveY = 6
-                moveX = -moveX
+                moveY = -(moveY- (ball.getCenter().getY() - lpaddleY)/14)
+                moveX = abs((15 - moveY**2)**0.5)
+                
             else:
-                global rScore
                 rScore = rScore + 1
                 moveX = 0
+                
         if not 10 < ball.getCenter().getY() < 590:
             moveY = -moveY
+            
         ballmove(ball, moveX, moveY)
+
+    time.sleep(0.2)
 
     ball.undraw()
     lpaddle.undraw()
@@ -109,9 +123,40 @@ def gameUX():
     rScoreDisplay.undraw()
     lScoreDisplay.undraw()
 
+def endGame ():
+    if lScore == 5:
+        leftWins = Text(Point(300, 150), "Left Wins!")    
+        leftWins.setFace("helvetica")
+        leftWins.setSize(36)
+        leftWins.setTextColor("white")
+        leftWins.draw(window)
+    if rScore == 5:
+        rightWins = Text(Point(300, 150), "Right Wins!")    
+        rightWins.setFace("helvetica")
+        rightWins.setSize(36)
+        rightWins.setTextColor("white")
+        rightWins.draw(window)
+    
+    lScoreDisplay = Text(Point(200, 50), lScore)
+    lScoreDisplay.setFace("helvetica")
+    lScoreDisplay.setSize(18)
+    lScoreDisplay.setTextColor("white")
+
+    rScoreDisplay = Text(Point(400, 50), rScore)
+    rScoreDisplay.setFace("helvetica")
+    rScoreDisplay.setSize(18)
+    rScoreDisplay.setTextColor("white")
+    
+    rScoreDisplay.draw(window)
+    lScoreDisplay.draw(window)
+
 def ballmove(ball, moveX, moveY):
     ball.move(moveX * 2, moveY * 2)
         
-splashUX()
-while lScore != 5 and rScore !=5:
-    gameUX()
+def newGame():
+    splashUX()
+    while lScore != 5 and rScore !=5:
+        gameUX()
+    endGame()
+
+newGame()
